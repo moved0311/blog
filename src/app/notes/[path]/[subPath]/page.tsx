be3e4aaff18d2fef7a7012f84e762d0cb83d73c2
taiyi <taiyijiang@anuegroup.com.tw>
2024-09-12 18:04:33 +0800

    feat: add notes and notes subroute

---
+++ src/app/notes/[path]/[subPath]/page.tsx
@@ -0,0 +1,55 @@
+import { format, parseISO } from "date-fns";
+import { allPosts } from "contentlayer/generated";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import { notFound } from "next/navigation";
+import { type Metadata } from "next";
+
+type PageProps = {
+  params: {
+    path: string;
+    subPath: string;
+  };
+};
+
+type Props = {
+  params: { path: string };
+  searchParams?: { [key: string]: string | string[] };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { path } = params;
+  const title = `${path} | Taiyi | Dev`;
+
+  return {
+    title,
+  };
+}
+
+const Page = ({ params }: PageProps) => {
+  const path = params?.path;
+  const subPath = params?.subPath;
+  const notesPath = `notes/${path}/${subPath}`;
+  const post = allPosts.find((post) => post._raw.flattenedPath === notesPath);
+
+  if (!post) notFound();
+
+  const { title, date, body } = post;
+
+  const MDXContent = useMDXComponent(body.code);
+
+  return (
+    <div className="dark:text-white">
+      <div className="mb-8">
+        <time dateTime={date} className="mb-1 text-xs text-gray-500">
+          {format(parseISO(date), "LLLL d, yyyy")}
+        </time>
+        <h1 className="text-3xl font-bold dark:text-white">{title}</h1>
+      </div>
+      <article className="prose md:prose-lg dark:prose-invert prose-a:no-underline prose-th:text-center prose-li:m-0 prose-ul:m-0 prose-h2:mb-0 prose-table:w-auto">
+        <MDXContent />
+      </article>
+    </div>
+  );
+};
+
+export default Page;
