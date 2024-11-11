d72eb0f5b17723721d2edbc571feec2dc903a748
taiyi <taiyijiang@anuegroup.com.tw>
2024-11-11 11:30:33 +0800

    feat: add Notes link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,10 +3,17 @@ import DarkModeSwitch from "@/components/DarkMode/index.client";
 
 const index = () => {
   return (
-    <Link href="/" className="flex items-center justify-between text-[#007acc]">
-      <h1 className="text-3xl font-extrabold py-8">Taiyi dev</h1>
-      <DarkModeSwitch />
-    </Link>
+    <div className="flex items-center justify-between text-[#007acc]">
+      <Link href="/">
+        <h1 className="text-3xl font-extrabold py-8">Taiyi dev</h1>
+      </Link>
+      <div className="flex items-center gap-3">
+        <Link href="/notes" className="font-bold">
+          Notes
+        </Link>
+        <DarkModeSwitch />
+      </div>
+    </div>
   );
 };
 
