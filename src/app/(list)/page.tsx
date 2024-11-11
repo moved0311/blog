d57904f4af74d34b55b95a64adaa100aa41cc331
taiyi <taiyijiang@anuegroup.com.tw>
2024-11-11 11:40:13 +0800

    feat: change post list sort function by last update

--- src/app/(list)/page.tsx
+++ src/app/(list)/page.tsx
@@ -3,9 +3,11 @@ import { allPosts } from "contentlayer/generated";
 import PostCard from "@/components/Post";
 
 export default function Home() {
-  const posts = allPosts.sort((a, b) =>
-    compareDesc(new Date(a.date), new Date(b.date)),
-  );
+  const posts = allPosts.sort((a, b) => {
+    const date_a = new Date(a.lastUpdate || a.date);
+    const date_b = new Date(b.lastUpdate || b.date);
+    return compareDesc(date_a, date_b);
+  });
 
   return (
     <div className="mx-auto max-w-3xl py-8">
