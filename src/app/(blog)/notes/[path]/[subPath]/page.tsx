3e3f71f55e21afe5139abe6e3474ef3a80902d86
Taiyi <moved0311@gmail.com>
2025-10-04 00:12:57 +0800

    feat: update japanese note

--- src/app/(blog)/notes/[path]/[subPath]/page.tsx
+++ src/app/(blog)/notes/[path]/[subPath]/page.tsx
@@ -46,7 +46,7 @@ const Page = ({ params }: PageProps) => {
         </time>
         <h1 className="text-3xl font-bold dark:text-white">{title}</h1>
       </div>
-      <article className="prose md:prose-lg dark:prose-invert prose-a:no-underline prose-th:text-center prose-li:m-0 prose-ul:m-0 prose-h2:mb-2 prose-table:w-auto">
+      <article className="prose md:prose-lg dark:prose-invert prose-a:no-underline prose-th:text-center prose-li:m-0 prose-ol:m-0 prose-ul:m-0 prose-h2:mb-2 prose-table:w-auto prose-p:m-1">
         <MDXContent />
       </article>
     </div>
